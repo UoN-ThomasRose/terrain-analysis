c01b15911514342f67d5b174ba8010382ee3178b
Tom <Thomas.Rose3@northampton.ac.uk>
2016-11-08 13:41:25 +0000

    0.1

---
+++ Assets/Editor/TerrainAnalysisEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor (typeof(TerrainAnalysis))]
+public class TerrainAnalysisEditor : Editor {
+
+    public TerrainAnalysis ta;
+
+	public override void OnInspectorGUI() {
+
+        // Get the Terrain Analysis class
+        ta = (TerrainAnalysis)target;
+
+        // If auto-update is turned on get the terrain metrics
+        if(ta.autoUpdate)
+            ta.GetMetrics();
+
+        // Custom GUI
+        EditorGUILayout.LabelField("Width", ta.width.ToString());
+        EditorGUILayout.LabelField("Length", ta.length.ToString());
+        EditorGUILayout.LabelField("Lowest Point", ta.minHeight.ToString());
+        EditorGUILayout.LabelField("Highest Point", ta.maxHeight.ToString());
+        EditorGUILayout.LabelField("Roughness", ta.roughness.ToString());
+        ta.autoUpdate = EditorGUILayout.Toggle("Auto Update", ta.autoUpdate);
+        if(GUILayout.Button("Refresh")) {
+            ta.GetMetrics();
+        }
+    }
+}
+
+
--- Assets/Scripts/TerrainAnalysis.cs
+++ Assets/Scripts/TerrainAnalysis.cs
@@ -3,19 +3,71 @@ using System.Collections;
 
 public class TerrainAnalysis : MonoBehaviour {
 
-    public int width;
-    public int length;
-    public int maxHeight;
-    public int minHeight;
-    public int roughness;
+    public float width;
+    public float length;
+    public float maxHeight;
+    public float minHeight;
+    public float roughness;
+    public bool autoUpdate = false;
+    public Terrain terrainMesh;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public TerrainAnalysis() {
+        terrainMesh = GetComponent<Terrain>();
+    }
+
+	public void GetMetrics() {
+
+        // Get the terrain data
+        TerrainData td = terrainMesh.terrainData;
+
+        // Get the heightmap and store it in a 2D float array
+        float[,] heightmap = td.GetHeights(0, 0, (int)width, (int)length);
+
+        /*
+         * ==========================
+         * ======= DIMENSIONS =======
+         * ==========================
+        */
+
+        // Return length (x) and width (y)
+        width = td.size.x;
+        length = td.size.z;
+        
+        /*
+         * ==========================
+         * ==== MAX & MIN HEIGHT ====
+         * ==========================
+        */ 
+
+        float tempMaxHeight = 0;
+        float tempMinHeight = td.size.y; // TODO: See if this actually works as intended
+
+        // loop through the whole heightmap
+        for(int x = 0; x < heightmap.GetLength(0); x++) {
+            for (int y = 0; y < heightmap.GetLength(1); y++) {
+                try {
+                    if (heightmap[x, y] > tempMaxHeight) // high check
+                        tempMaxHeight = heightmap[x, y];
+                    if (heightmap[x, y] < tempMinHeight) // low check
+                        tempMinHeight = heightmap[x, y];
+                } catch (System.Exception e) {
+                    Debug.Log(e);
+                    Debug.Log("X: " + x + ". Y: " + y + ".");
+                }
+            }
+        }
+
+        // assign the values
+        maxHeight = tempMaxHeight * td.size.y;
+        minHeight = tempMinHeight * td.size.y;
+
+        /*
+         * ===========================
+         * ======== ROUGHNESS ========
+         * ===========================
+        */
+
+        // TODO: Add this!
+    }
 }
+
