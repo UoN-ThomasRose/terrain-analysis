254d2293796ad63bd3332d19505b22982ddb78b7
Tom <Thomas.Rose3@northampton.ac.uk>
2016-11-08 11:54:33 +0000

    Initial Commit

---
+++ Assets/Scripts/TerrainAnalysis.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainAnalysis : MonoBehaviour {
+
+    public int width;
+    public int length;
+    public int maxHeight;
+    public int minHeight;
+    public int roughness;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
