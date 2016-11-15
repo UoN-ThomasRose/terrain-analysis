cd423ccc8204d87040bba4a3c278f4736165c802
Tom <Thomas.Rose3@northampton.ac.uk>
2016-11-15 13:44:54 +0000

    0.2

--- Assets/Editor/TerrainAnalysisEditor.cs
+++ Assets/Editor/TerrainAnalysisEditor.cs
@@ -17,11 +17,27 @@ public class TerrainAnalysisEditor : Editor {
             ta.GetMetrics();
 
         // Custom GUI
+        EditorGUILayout.LabelField("Dimensions", EditorStyles.boldLabel);
         EditorGUILayout.LabelField("Width", ta.width.ToString());
         EditorGUILayout.LabelField("Length", ta.length.ToString());
-        EditorGUILayout.LabelField("Lowest Point", ta.minHeight.ToString());
-        EditorGUILayout.LabelField("Highest Point", ta.maxHeight.ToString());
-        EditorGUILayout.LabelField("Roughness", ta.roughness.ToString());
+
+        EditorGUILayout.LabelField("Height Properties", EditorStyles.boldLabel);
+        EditorGUILayout.LabelField("Min Height", ta.minHeight.ToString());
+        EditorGUILayout.LabelField("Max Height", ta.maxHeight.ToString());
+        EditorGUILayout.LabelField("Lowest Point", ta.highestPoint.ToString());
+        EditorGUILayout.LabelField("Highest Point", ta.lowestPoint.ToString());
+
+        EditorGUILayout.LabelField("Roughness Properties", EditorStyles.boldLabel);
+        EditorGUILayout.LabelField("Arithmetic Average", ta.arithmeticAvg.ToString());
+        EditorGUILayout.LabelField("Root Mean Squared", ta.rootMeanSquared.ToString());
+        EditorGUILayout.LabelField("Skewness", ta.skewness.ToString());
+        EditorGUILayout.LabelField("Kurtosis", ta.kurtosis.ToString());
+
+        EditorGUILayout.LabelField("Material Properties", EditorStyles.boldLabel);
+        EditorGUILayout.LabelField("Rock Exposure", ta.rockExposure.ToString());
+        EditorGUILayout.LabelField("Soil Type", ta.soilType);
+
+        EditorGUILayout.LabelField("Update Options", EditorStyles.boldLabel);
         ta.autoUpdate = EditorGUILayout.Toggle("Auto Update", ta.autoUpdate);
         if(GUILayout.Button("Refresh")) {
             ta.GetMetrics();
--- Assets/Scripts/TerrainAnalysis.cs
+++ Assets/Scripts/TerrainAnalysis.cs
@@ -1,73 +1,106 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
 
 public class TerrainAnalysis : MonoBehaviour {
 
+    // Terrain Basic Data
     public float width;
     public float length;
-    public float maxHeight;
     public float minHeight;
-    public float roughness;
+    public float maxHeight;
+
+    // Terrain Roughness 
+    public float lowestPoint;
+    public float highestPoint;
+    public float arithmeticAvg; 
+    public float rootMeanSquared;
+    public float skewness;
+    public float kurtosis;
+
+    // Terrain Material
+    public float rockExposure; // TODO: Review this! Should this be implemented? Is it general enough?
+    public string soilType = null; // TODO: Review this! Should this be implemented through a data structure or class or left out completely?!
+
+    // Program Data
     public bool autoUpdate = false;
     public Terrain terrainMesh;
-
-    public TerrainAnalysis() {
-        terrainMesh = GetComponent<Terrain>();
-    }
 
 	public void GetMetrics() {
 
         // Get the terrain data
+        terrainMesh = GetComponent<Terrain>();
         TerrainData td = terrainMesh.terrainData;
 
         // Get the heightmap and store it in a 2D float array
         float[,] heightmap = td.GetHeights(0, 0, (int)width, (int)length);
 
         /*
-         * ==========================
-         * ======= DIMENSIONS =======
-         * ==========================
+         * ===========================================
+         * ======= DIMENSIONS AND HEIGHT RANGE =======
+         * ===========================================
         */
 
         // Return length (x) and width (y)
         width = td.size.x;
         length = td.size.z;
+
+        // Return height range
+        minHeight = 0; // TODO: Isn't this always the case? Is this neccessary?
+        maxHeight = td.size.y;
         
         /*
-         * ==========================
-         * ==== MAX & MIN HEIGHT ====
-         * ==========================
+         * ============================================
+         * ==== HIGHEST/LOWEST POINT AND ROUGHNESS ====
+         * ============================================
         */ 
 
         float tempMaxHeight = 0;
-        float tempMinHeight = td.size.y; // TODO: See if this actually works as intended
+        float tempMinHeight = td.size.y;
+        float heightSum = 0;
+        float squaredHeightSum = 0;
+        float cubedHeightSum = 0;
+        float quartedHeightSum = 0;
 
         // loop through the whole heightmap
-        for(int x = 0; x < heightmap.GetLength(0); x++) {
+        for (int x = 0; x < heightmap.GetLength(0); x++) {
             for (int y = 0; y < heightmap.GetLength(1); y++) {
-                try {
-                    if (heightmap[x, y] > tempMaxHeight) // high check
-                        tempMaxHeight = heightmap[x, y];
-                    if (heightmap[x, y] < tempMinHeight) // low check
-                        tempMinHeight = heightmap[x, y];
-                } catch (System.Exception e) {
-                    Debug.Log(e);
-                    Debug.Log("X: " + x + ". Y: " + y + ".");
-                }
+                if (heightmap[x, y] > tempMaxHeight) // high check
+                    tempMaxHeight = heightmap[x, y];
+                if (heightmap[x, y] < tempMinHeight) // low check
+                    tempMinHeight = heightmap[x, y];
+                heightSum += heightmap[x, y] * td.size.y;
+                squaredHeightSum += (float) Math.Pow(heightmap[x, y] * td.size.y, 2);
+                cubedHeightSum += (float) Math.Pow(heightmap[x, y] * td.size.y, 3);
+                quartedHeightSum += (float) Math.Pow(heightmap[x, y] * td.size.y, 4);
+
             }
         }
 
+        int n = heightmap.GetLength(0) * heightmap.GetLength(1);
+
         // assign the values
-        maxHeight = tempMaxHeight * td.size.y;
-        minHeight = tempMinHeight * td.size.y;
+        lowestPoint = tempMaxHeight * td.size.y;
+        highestPoint = tempMinHeight * td.size.y;
+        arithmeticAvg = heightSum / n;
+        rootMeanSquared = (float) Math.Sqrt(squaredHeightSum / n);
+        skewness = cubedHeightSum / (n * (float) Math.Pow(rootMeanSquared, 3));
+        kurtosis = quartedHeightSum / (n * (float) Math.Pow(rootMeanSquared, 4));
+
 
         /*
-         * ===========================
-         * ======== ROUGHNESS ========
-         * ===========================
+         * ==========================
+         * ======== MATERIAL ========
+         * ==========================
         */
 
         // TODO: Add this!
+
+        /*
+         *  Rock Exposure (RE) | Rockiness Factor (rf) | Slope (s)
+         *  re = rf * s ?
+         *  
+         *  Soil Type represented by a class (class could contain textures)?
+        */
     }
 }
 
